0891f98f5c4d99d99a2daf04c612995fce2628dd
Ivan33190689 <museiq@yandex.ru>
2023-11-15 04:44:17 +0200

    Sum

--- Example_001CSHARP/Program.cs
+++ Example_001CSHARP/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Введите ваше имя");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿// Console.WriteLine("Введите ваше имя");
+// string username = Console.ReadLine();
+// Console.Write("Привет, ");
+// Console.Write(username);
+
+int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
