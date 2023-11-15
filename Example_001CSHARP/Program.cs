289938ef8c493c49ac255976ba9740c46bcd7942
Ivan33190689 <museiq@yandex.ru>
2023-11-15 06:06:02 +0200

    Random

--- Example_001CSHARP/Program.cs
+++ Example_001CSHARP/Program.cs
@@ -6,8 +6,13 @@
 // int numberA = 3;
 // int numberB = 5;
 // int result = numberA + numberB;
-// Console.WriteLine(result);
+// Console.WriteLe(result);
 
-double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+// double numberA = 12;
+// double numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
+int result = numberA + numberB;
+Console.WriteLine($"{numberA} + {numberB} = {result}");
