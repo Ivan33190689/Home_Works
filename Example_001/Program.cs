b558b1121e7ae61e7bca01eb2aef9cb43a2d2935
Ivan33190689 <museiq@yandex.ru>
2023-11-17 13:23:05 +0200

    Большее чисдо из двуч

---
+++ Example_001/Program.cs
@@ -0,0 +1,25 @@
+﻿// Большее из двух чисел.
+
+void CompareNumbers(int firstNumber, int secondNumber)
+   {
+        if (firstNumber == secondNumber)
+        {
+            Console.WriteLine($"Введенные числа равны `{firstNumber}`");
+        }
+        if (firstNumber > secondNumber)
+        {
+            Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
+        }
+        if (firstNumber < secondNumber)
+        {
+            Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
+        }
+    }
+
+Console.WriteLine("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+CompareNumbers(firstNumber, secondNumber);
+Console.WriteLine();
