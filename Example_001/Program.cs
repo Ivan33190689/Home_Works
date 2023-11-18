0c1b202fed6256b4838d2ed64ac9037737e87bc0
Ivan33190689 <museiq@yandex.ru>
2023-11-18 13:41:36 +0200

    Нахождение максимального

--- Example_001/Program.cs
+++ Example_001/Program.cs
@@ -1,25 +1,48 @@
 ﻿// Большее из двух чисел.
 
-void CompareNumbers(int firstNumber, int secondNumber)
-   {
-        if (firstNumber == secondNumber)
-        {
-            Console.WriteLine($"Введенные числа равны `{firstNumber}`");
-        }
-        if (firstNumber > secondNumber)
-        {
-            Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
-        }
-        if (firstNumber < secondNumber)
-        {
-            Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
-        }
-    }
+// void CompareNumbers(int firstNumber, int secondNumber)
+//    {
+//         if (firstNumber == secondNumber)
+//         {
+//             Console.WriteLine($"Введенные числа равны `{firstNumber}`");
+//         }
+//         if (firstNumber > secondNumber)
+//         {
+//             Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
+//         }
+//         if (firstNumber < secondNumber)
+//         {
+//             Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
+//         }
+//     }
+
+// Console.WriteLine("Введите первое число: ");
+// int firstNumber = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите второе число: ");
+// int secondNumber = int.Parse(Console.ReadLine()!);
+
+// CompareNumbers(firstNumber, secondNumber);
+// Console.WriteLine();
+
+
+// Нахождение максимального из трёх.
+
+int FindMax(int a, int b, int c)
+{
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    return max;
+}
 
 Console.WriteLine("Введите первое число: ");
-int firstNumber = int.Parse(Console.ReadLine()!);
+int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе число: ");
-int secondNumber = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
 
-CompareNumbers(firstNumber, secondNumber);
-Console.WriteLine();
+int result = FindMax(a, b, c);
+Console.WriteLine($"Максимальное из трёх чисел = [ {result} ]");
+
+
