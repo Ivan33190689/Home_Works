99bade3378c58e09bac4f26e34928b2faf1075db
Ivan33190689 <museiq@yandex.ru>
2023-11-21 00:03:01 +0200

    Кратность 23 и 7

---
+++ Example_002/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход 
+// число и проверяет, кратно ли оно одновременно 7 и 23.
+
+void MultiNumber(int number)
+{
+    if (number % 7 == 0 && number % 23 == 0)
+    {
+        System.Console.WriteLine($"Чисдо {number} кратное 7 и 23");
+    }
+    else
+    {
+        System.Console.WriteLine($"Число {number} некратное 23 и 7");
+    }
+}
+
+System.Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+MultiNumber(number);
+System.Console.WriteLine();
