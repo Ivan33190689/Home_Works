cd901666e1ce9540f6359783eb4758bcf35c7ed9
Ivan33190689 <museiq@yandex.ru>
2023-11-20 16:47:59 +0200

    Нахождение max в массиве

--- Example_002CSHARP/Program.cs
+++ Example_002CSHARP/Program.cs
@@ -6,18 +6,25 @@
     return result;
 }
 
-int a1 = 4;
-int b1 = 5;
-int c1 = 16;
-int a2 = 1;
-int b2 = 9;
-int c2 = 52;
-int a3 = 63;
-int b3 = 26;
-int c3 = 61;
+// int a1 = 4;
+// int b1 = 5;
+// int c1 = 16;
+// int a2 = 1;
+// int b2 = 9;
+// int c2 = 52;
+// int a3 = 63;
+// int b3 = 26;
+// int c3 = 61;
 
-int max1 = MaxNumber(a1, b1, c1);
-int max2 = MaxNumber(a2, b2, c2);
-int max3 = MaxNumber(a3, b3, c3);
-int max = MaxNumber(max1, max2, max3);
+// int max1 = MaxNumber(a1, b1, c1);
+// int max2 = MaxNumber(a2, b2, c2);
+// int max3 = MaxNumber(a3, b3, c3);
+// int max = MaxNumber(max1, max2, max3);
+
+// System.Console.WriteLine(max);
+
+int[] array = {35, 28, 71, 28, 48, 72, 85, 58, 47};
+
+int max = MaxNumber(MaxNumber(array[0], array[1], array[2]), MaxNumber(array[3], array[4], array[5]), MaxNumber(array[6], array[7], array[8]));
+
 System.Console.WriteLine(max);
