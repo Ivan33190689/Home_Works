7952defa14f90c0d3d38c73a601eb816630238f3
Ivan33190689 <museiq@yandex.ru>
2023-11-25 18:12:02 +0200

    Количество чётных чисел

--- Example_003/Program.cs
+++ Example_003/Program.cs
@@ -2,15 +2,56 @@
 // от 1 до 100. Найдите количество элементов массива, 
 // значения которых лежат в отрезке [20,90].
 
-using System.ComponentModel;
+
+// int[] NewArray()
+// {
+//     int[] newArray = new int[10];
+
+//     Random rand = new Random();
+//     for (int i = 0; i < 10; i++)
+//         newArray[i] = rand.Next(1, 101);
+//     return newArray;
+// }
+
+// void PrintArray(int[] newArray)
+// {
+//     for (int i = 0; i < newArray.Length; i++)
+//         System.Console.Write(newArray[i] + " ");
+//     System.Console.WriteLine();
+// }
+
+// int[] array = NewArray();
+// PrintArray(array);
+
+// int QuantityNumbers(int[] array)
+// {
+//     int begin = 20;
+//     int end = 90;
+//     int quantity = 0;
+//     for (int i = 0; i < array.Length; i++)
+
+//         if (array[i] >= begin && array[i] <= end)
+//             quantity++;
+
+//     return quantity;
+// }
+
+// int quantity = QuantityNumbers(array);
+// System.Console.WriteLine($"Количество чиcел в отрезке [20, 90] = {quantity}");
+
+
+// Задача 2: Задайте массив на 10 целых чисел. 
+// Напишите программу, которая определяет количество 
+// чётных чисел в массиве.
 
 int[] NewArray()
 {
     int[] newArray = new int[10];
-
     Random rand = new Random();
+
     for (int i = 0; i < 10; i++)
         newArray[i] = rand.Next(1, 101);
+
     return newArray;
 }
 
@@ -24,20 +65,18 @@ void PrintArray(int[] newArray)
 int[] array = NewArray();
 PrintArray(array);
 
-int QuantityNumbers(int[] array)
+int EvenNumbers(int[] array)
 {
-    int begin = 20;
-    int end = 90;
-    int quantity = 0;
+    int count = 0;
     for (int i = 0; i < array.Length; i++)
-    
-        if (array[i] >= begin && array[i] <= end)
-            quantity++;
-    
-    return quantity;
+
+    if (array[i] % 2 == 0)
+        count++;
+
+    return count;
 }
 
-int quantity = QuantityNumbers(array);
-System.Console.WriteLine($"Количество чиcел в отрезке [20, 90] = {quantity}");
+int evenNumbers = EvenNumbers(array);
+System.Console.WriteLine($"Количество чётных чисел = {evenNumbers}");
 
 
