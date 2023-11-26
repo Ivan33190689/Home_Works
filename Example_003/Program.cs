afb05844506cf797bbbff8b709635d2d86e74834
Ivan33190689 <museiq@yandex.ru>
2023-11-27 01:47:36 +0200

    Разница между max и min элементами массива

--- Example_003/Program.cs
+++ Example_003/Program.cs
@@ -44,39 +44,66 @@
 // Напишите программу, которая определяет количество 
 // чётных чисел в массиве.
 
-int[] NewArray()
+// int[] NewArray()
+// {
+//     int[] newArray = new int[10];
+//     Random rand = new Random();
+
+//     for (int i = 0; i < 10; i++)
+//         newArray[i] = rand.Next(1, 101);
+
+//     return newArray;
+// }
+
+// void PrintArray(int[] newArray)
+// {
+//     for (int i = 0; i < newArray.Length; i++)
+//         System.Console.Write(newArray[i] + " ");
+//     System.Console.WriteLine();
+// }
+
+// int[] array = NewArray();
+// PrintArray(array);
+
+// int EvenNumbers(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+
+//     if (array[i] % 2 == 0)
+//         count++;
+
+//     return count;
+// }
+
+// int evenNumbers = EvenNumbers(array);
+// System.Console.WriteLine($"Количество чётных чисел = {evenNumbers}");
+
+
+// Задача 3: Задайте массив из вещественных чисел 
+// с ненулевой дробной частью. Найдите разницу между 
+// максимальным и минимальным элементов массива.
+
+
+void DifferenceMinMax()
 {
-    int[] newArray = new int[10];
-    Random rand = new Random();
+    double[] newArray = {2.5, 3.7, 5.9, 4.2, 1.3, 2.1};
+    double min = newArray[0];
+    double max = newArray[0];
+    double difference = 0;
+    int i = 1; 
 
-    for (int i = 0; i < 10; i++)
-        newArray[i] = rand.Next(1, 101);
+    while (i < newArray.Length)
+    {
+        if (min > newArray[i])
+            min = newArray[i];
+        if (max < newArray[i])
+            max = newArray[i];
+        i++;
+    }
+    difference = max - min;
+    System.Console.Write($"Разница между максимальным и минимальным элементов массива: {difference:F1}");
 
-    return newArray;
 }
 
-void PrintArray(int[] newArray)
-{
-    for (int i = 0; i < newArray.Length; i++)
-        System.Console.Write(newArray[i] + " ");
-    System.Console.WriteLine();
-}
-
-int[] array = NewArray();
-PrintArray(array);
-
-int EvenNumbers(int[] array)
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-
-    if (array[i] % 2 == 0)
-        count++;
-
-    return count;
-}
-
-int evenNumbers = EvenNumbers(array);
-System.Console.WriteLine($"Количество чётных чисел = {evenNumbers}");
-
-
+DifferenceMinMax();
