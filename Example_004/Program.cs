0b41617fb4aedd184c92d9228f99cdb826d24786
Ivan33190689 <museiq@yandex.ru>
2023-11-28 17:57:04 +0200

    Программа завершения работы по 'q' и по sum чётных чисел

---
+++ Example_004/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 1: Напишите программу, которая бесконечно 
+// запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ 
+// или при вводе числа, сумма цифр которого чётная.
+
+void OutputProgram()
+{
+    System.Console.Write("Введите 'q' или число для выхода: ");
+    string input = Console.ReadLine();
+
+    if (input == "q")
+    {
+        System.Console.WriteLine("[STOP]");
+        return;
+    }
+
+    int number;
+    
+    if (int.TryParse(input, out number))
+    {
+        int sum = 0;
+        
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        if (sum % 2 == 0)
+        {
+            System.Console.WriteLine("[STOP]");
+            return;
+        }
+        else
+        {
+            System.Console.WriteLine("Некорректный ввод. Введите целое число или 'q': ");
+        }
+    }
+}
+
+OutputProgram();
